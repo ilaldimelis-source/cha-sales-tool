dc0969a85bd75675fc0a7b32a3a926ecbbffba1d
ilaldimelis-source <ilaldimelis@gmail.com>
2026-04-02 09:49:18 -0400

    Add Stylelint config

---
+++ stylelint.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard'],
+  rules: {
+    'selector-class-pattern': null,
+    'alpha-value-notation': null,
+    'color-function-notation': null
+  }
+};
